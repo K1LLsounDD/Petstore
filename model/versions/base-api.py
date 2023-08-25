05b3142a45e48d5effc53cc3f6f5640e78deedd4
K1LLsounD <sgol99@mail.ru>
2023-08-25 21:13:34 +0300

    add base_api

---
+++ model/versions/base-api.py
@@ -0,0 +1,28 @@
+import requests
+from requests import Response
+
+
+class BaseApi:
+
+    @staticmethod
+    def get(path):
+        response: Response = requests.get(path)
+        return response
+
+
+    @staticmethod
+    def post(path, body):
+        response: Response = requests.post(path, json=body)
+        return response
+
+
+    @staticmethod
+    def put(path, body):
+        response: Response = requests.put(path, json=body)
+        return response
+
+
+    @staticmethod
+    def delete(path):
+        response: Response = requests.delete(path)
+        return response
